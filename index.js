19abee234726d919282189ee45d39f2dd4111b46
SatyamGade <131511270+SatyamGade@users.noreply.github.com>
2024-03-16 10:36:36 +0530

    index.js

--- index.js
+++ index.js
@@ -9,24 +9,12 @@ const authRoutes = require("./routes/authRoutes");
 const noteRoutes = require("./routes/noteRoutes");
 
  const corsOptions = {
-     origin: ["https://i-notebook-frontend-six.vercel.app"],
+     origin: "https://i-notebook-frontend-six.vercel.app/",
      methods: ["GET, POST, PUT, DELETE"],
      credentials: true
  }
 
 app.use(cors(corsOptions));
-app.use((req, res, next) => {
-      res.setHeader("Access-Control-Allow-Origin", "*");
-      res.setHeader(
-        "Access-Control-Allow-Methods",
-        "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-      );
-      res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-      if (req.method === "OPTIONS") {
-        return res.sendStatus(200);
-      }
-      next();
-    });
 
 app.use(express.json());
 app.use(errorMiddleware);
