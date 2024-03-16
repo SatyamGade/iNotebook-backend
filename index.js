83405024459933d245da6d12ec3d79474b137ce6
SatyamGade <131511270+SatyamGade@users.noreply.github.com>
2024-03-16 10:31:46 +0530

    index.js

--- index.js
+++ index.js
@@ -15,11 +15,18 @@ const noteRoutes = require("./routes/noteRoutes");
  }
 
 app.use(cors(corsOptions));
-app.use(function (req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   next();
-})
+app.use((req, res, next) => {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader(
+        "Access-Control-Allow-Methods",
+        "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+      );
+      res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+      if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+      }
+      next();
+    });
 
 app.use(express.json());
 app.use(errorMiddleware);
